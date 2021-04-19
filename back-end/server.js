40a06700b12eff0f175a48c719d4396b125fdb97
shafiimam <shafi15-8072@diu.edu.bd>
2021-04-20 03:37:59 +0600

    final deploy

--- back-end/server.js
+++ back-end/server.js
@@ -33,7 +33,7 @@ app.get('/api/config/paypal', (req, res)=> res.send(process.env.PAYPAL_CLIENT_ID
 
 
 const __dirname = path.resolve()
-app.use('/uploads',express.static(path.join(__dirname, '/uploads')))
+app.use('/Uploads',express.static(path.join(__dirname, '/Uploads')))
 
 if(process.env.NODE_ENV === 'PRODUCTION'){
     app.use(express.static(path.join(__dirname, '/front-end/build')))
